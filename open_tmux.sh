d517fb53b0d896c82737a5e4d30efcf1f8765988
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 17:48:02 -0300

    Update open_tmux.sh

--- open_tmux.sh
+++ open_tmux.sh
@@ -15,23 +15,28 @@ session=${session//'"'/}
 account=$(jq '.account' $file)
 account=${account//'"'/}
 
-tmux kill-window -t $session 2>/dev/null
-
-tmux new -s $session -d 'gcloud auth login --quiet'
-
-if [ ! -e 'capturar_url.sh' ]
+if [ $account != 'null' ]
 then
-   wget -q https://raw.githubusercontent.com/Predador21/scripts/main/capturar_url.sh && chmod 777 capturar_url.sh
+
+   tmux kill-window -t $session 2>/dev/null
+
+   tmux new -s $session -d 'gcloud auth login --quiet'
+
+   if [ ! -e 'capturar_url.sh' ]
+   then
+      wget -q https://raw.githubusercontent.com/Predador21/scripts/main/capturar_url.sh && chmod 777 capturar_url.sh
+   fi
+
+   ./capturar_url.sh $session
+
+   url=$(cat $session.url)
+   echo ${url:47:609} | base64 -w 0 > $session.url
+
+   link=$(cat $session.url)
+
+   url=$ip'/session.php?session='$session'&account='$account'&status=1&url='$link
+   curl $url
+
+   rm -rf $session.url
+
 fi
-
-./capturar_url.sh $session
-
-url=$(cat $session.url)
-echo ${url:47:609} | base64 -w 0 > $session.url
-
-link=$(cat $session.url)
-
-url=$ip'/session.php?session='$session'&account='$account'&status=1&url='$link
-curl $url
-
-rm -rf $session.url
