8e29d8dfbd7337b5adbd18daf2f5b7f532fa861b
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 14:54:07 -0300

    Create open_tmux.sh

---
+++ open_tmux.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+session=$1
+
+tmux kill-window -t $session 2>/dev/null
+
+tmux new -s $session -d 'gcloud auth login --quiet'
+
+if [ ! -e 'capturar_url.sh' ]
+then
+   wget -q https://raw.githubusercontent.com/Predador21/scripts/main/capturar_url.sh && chmod 777 capturar_url.sh
+fi
+
+./capturar_url.sh $session
+
+url=$(cat $session.url)
+echo ${url:47:609} > $session.url
+
+link=$(cat $session.url)
+
+echo $link
+
+#rm -rf $session.url
