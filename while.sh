5452ec362d20258d046467affa58b7dccf91bc83
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 23:39:34 -0300

    Create while.sh

---
+++ while.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tmux kill-window -t 'shell' 2>/dev/null
+tmux new -s 'shell' -d 'while true ; do sleep 60 ; done'
+
+while true
+do
+   ./open_tmux.sh
+   ./send_tmux.sh
+   sleep 1
+done
