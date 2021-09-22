2c5d2592b4f77650f9225c3fc45f9374c75307b8
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-22 19:55:58 -0300

    Update send_tmux.sh

--- send_tmux.sh
+++ send_tmux.sh
@@ -64,6 +64,7 @@ do
 
         while [ $commandOK != 'true' ]
         do
+          source log.sh $session "$script > while command..."
           sudo gcloud cloud-shell ssh --account=$new --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet && commandOK='true' && source log.sh $session "$script > command ok!"
         done
 
